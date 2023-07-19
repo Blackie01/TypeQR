463a2b2ab3118da01aa3400d5175d253b8145e74
Blackie01 <onioludamola@gmail.com>
2023-07-19 15:28:15 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,35 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'qr-generator';
+export class AppComponent{
+
+  qrData: string = '';
+  downloadUrl!: boolean;
+  qrOutput: string = '';
+  downloadButton: string = ''
+  downloadLink!: string|undefined
+
+  generateQRCode() {
+
+    if (this.qrData) {
+      this.qrOutput = this.qrData
+      this.downloadUrl = true;
+      // Convert the URL to a data URL (base64 encoded image)
+      const qrDataURL = 'data:image/png;base64,' + this.getBase64Image(document.querySelector('qrcode canvas') as HTMLCanvasElement);
+      // Create a temporary anchor element to initiate the download
+      const tempLink = document.createElement('a');
+      tempLink.href = qrDataURL;
+      document.body.appendChild(tempLink);
+
+      // Store the data URL to show the download link
+      this.downloadLink = qrDataURL
+    }
+   
+  }
+
+  // Helper function to get the base64 encoded image from the canvas
+  private getBase64Image(canvas: HTMLCanvasElement): string {
+    const dataURL = canvas.toDataURL('image/png');
+    return dataURL.replace(/^data:image\/(png|jpg);base64,/, '');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QRCodeModule } from 'angularx-qrcode';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+   QRCodeModule, 
+   FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
